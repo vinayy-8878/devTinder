c8dfdeab8bc3f6b5fd1b604d831c8715cdf8e3d6
Vinay <vinayrathi786@gmail.com>
2024-11-22 20:06:44 +0530

    exploring api calls

--- src/app.js
+++ src/app.js
@@ -7,3 +7,12 @@ app.use("/test",(req,res)=>{
 app.use("/hello",(req,res)=>{
     res.send("hello server started")
 })
+app.listen(7777,()=>{
+    console.log("server started successfully")
+})
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Vinay"});
+})
+app.post("/user",(req,res)=>{
+    res.send("Data added successfully")
+})
