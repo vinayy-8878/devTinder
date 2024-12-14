63f2fac9a9395b0955044a85aad47c273c491f3c
Vinay <vinayrathi786@gmail.com>
2024-12-15 00:55:20 +0530

    connection api

--- src/Routes/requests.js
+++ src/Routes/requests.js
@@ -1,5 +1,47 @@
 const express = require("express");
 const requestRouter = express.Router();
+const ConnectionRequest = require("../models/connectionRequest");
+const { userAuth } = require("../utils/userAuth");
 const User = require("../models/user");
-const {userAuth}=require("../utils/userAuth");
-module.exports=requestRouter;
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+      const allowedStatus = ["interested", "ignored"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+const toUser=await User.findOne({_id:toUserId})
+if(!toUser){
+    throw new Error("User does not exists!.")
+}
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+       
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+       return res.status(404).json({ message: "Connection request already exists!." });
+      }
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({ message:req.user.firstName+" is "+status+" in "+toUser.firstName, data });
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+);
+
+module.exports = requestRouter;
---
+++ src/models/connectionRequest.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const connectionSchema = new mongoose.Schema(
+  {
+  fromUserId:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+  },
+  toUserId:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+  },
+  status:{
+    type:String,
+    required:true,
+    enum:{
+        values:["ignored","accepted","rejected","interested"],
+        message:` {VALUE} is incorrect status type.`
+    }
+  }
+  },
+  { timestamps: true }
+);
+connectionSchema.pre("save",function (next){
+  const connectionRequestFromAndTo=this;
+  if(connectionRequestFromAndTo.fromUserId.equals(connectionRequestFromAndTo.toUserId)){
+    throw new Error("You cannnot send request to yourself.")
+  }
+})
+
+module.exports = mongoose.model("ConnectionRequest", connectionSchema);
